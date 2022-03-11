0d6da093fc07a8fa7287f663deb5b9865cde9066
jaha2600 <jasmine.hansen@colorado.edu>
2022-03-11 10:59:16 -0700

    gnss trend plotting

---
+++ gnss/gnet_gnss_timeseries.py
@@ -0,0 +1,32 @@
+
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+#%%
+# import data
+ds = pd.read_csv('/Users/jasminehansen/Documents/Colorado/PhD_Work/2022/Greenland/DATASETS/GNET/KELY_NEU.csv')
+# %%
+plt.scatter(ds['Time'], ds['Up'])
+# %%
+import numpy as np
+
+#fit polynomial models up to degree 5
+model1 = np.poly1d(np.polyfit(ds.Time, ds.Up, 1))
+model2 = np.poly1d(np.polyfit(ds.Time, ds.Up, 2))
+model3 = np.poly1d(np.polyfit(ds.Time, ds.Up, 3))
+model4 = np.poly1d(np.polyfit(ds.Time, ds.Up, 4))
+model5 = np.poly1d(np.polyfit(ds.Time, ds.Up, 5))
+#%%
+#create scatterplot
+polyline = np.linspace(1, 15, 50)
+plt.scatter(ds.Time, ds.Up)
+
+#add fitted polynomial lines to scatterplot 
+plt.plot(polyline, model1(polyline), color='green')
+plt.plot(polyline, model2(polyline), color='red')
+plt.plot(polyline, model3(polyline), color='purple')
+plt.plot(polyline, model4(polyline), color='blue')
+plt.plot(polyline, model5(polyline), color='orange')
+plt.show()
+
+# %%
