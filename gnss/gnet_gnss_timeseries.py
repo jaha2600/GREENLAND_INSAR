ab9a65cea3443c57bff567909823d8db219fa2bf
jaha2600 <jasmine.hansen@colorado.edu>
2022-04-12 13:52:37 -0600

    mintpy_configs

--- gnss/gnet_gnss_timeseries.py
+++ gnss/gnet_gnss_timeseries.py
@@ -2,34 +2,40 @@
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
+#from sklearn.metrics import r2_score
+import numpy as np
 #%%
 # import data
-ds = pd.read_csv('/data/GREENLAND/2022/GNET_TIMESERIES/KELY_NEU.csv')
+ds = pd.read_csv('/Users/jasminehansen/Documents/Colorado/PhD_Work/2022/Greenland/DATASETS/GNET/KELY_NEU.csv')
 # %%
-
+#get correct data timeframe
+df = ds.loc[6482:]
+#df = ds
 #add 
 
-plt.scatter(ds['Time'], ds['Up'])
+x = np.arange(df['Time'].size) # = array([0, 1, 2, ..., 3598, 3599, 3600])
+fit = np.polyfit(x, df['Up'], 1)
+fit_fn = np.poly1d(fit)
+model2 = np.poly1d(np.polyfit(x, df.Up, 2))
+model3 = np.poly1d(np.polyfit(x, df.Up, 3))
+model4 = np.poly1d(np.polyfit(x, df.Up, 4))
+model5 = np.poly1d(np.polyfit(x, df.Up, 5))
+
+reg_coeff = fit[0]
+inters = fit[1]
+print('Line eq = y = {} * x + {}'.format(reg_coeff, inters))
+
+#r2_score(y, predict(x))
+
+# timeseries
+plt.plot(df['Time'], df['Up'], 'go', ms=2)
+# linear
+plt.plot(df['Time'], fit_fn(x), 'k-')
+#2
+#plt.plot(df['Time'], model2(x), 'b-')
+#plt.plot(df['Time'], model3(x), 'b-')
+#plt.plot(df['Time'], model4(x), 'b-')
+#plt.plot(df['Time'], model5(x), 'b-')
 # %%
-import numpy as np
-
-#fit polynomial models up to degree 5
-model1 = np.poly1d(np.polyfit(ds.Time, ds.Up, 1))
-model2 = np.poly1d(np.polyfit(ds.Time, ds.Up, 2))
-model3 = np.poly1d(np.polyfit(ds.Time, ds.Up, 3))
-model4 = np.poly1d(np.polyfit(ds.Time, ds.Up, 4))
-model5 = np.poly1d(np.polyfit(ds.Time, ds.Up, 5))
-#%%
-#create scatterplot
-polyline = np.linspace(1, 15, 50)
-plt.scatter(ds.Time, ds.Up)
-
-#add fitted polynomial lines to scatterplot 
-plt.plot(polyline, model1(polyline), color='green')
-plt.plot(polyline, model2(polyline), color='red')
-plt.plot(polyline, model3(polyline), color='purple')
-plt.plot(polyline, model4(polyline), color='blue')
-plt.plot(polyline, model5(polyline), color='orange')
-plt.show()
 
 # %%
