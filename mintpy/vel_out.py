f4361efbfca1be2b4ac2030229d084b5819262b3
jaha2600 <jasmine.hansen@colorado.edu>
2022-04-25 11:59:16 -0600

    few

--- mintpy/vel_out.py
+++ mintpy/vel_out.py
@@ -8,10 +8,10 @@ from mintpy import view, plot_network
 from mintpy.unwrap_error_phase_closure import plot_num_triplet_with_nonzero_integer_ambiguity
 
 # %%
-dirs = '/home/jasmine/JasmineShare/kanger/mintpy/'
+dirs = '/Users/jasminehansen/Documents/Colorado/PhD_Work/2022/Greenland/INSAR/kanger_mintpy_full/'
 os.chdir(dirs)
 #%%
-view.main('geo/geo_velocity.h5 velocity -v -0.5 0.5 --noverbose -c BlueWhiteOrangeRed'.split())
+view.main('geo_velocity.h5 velocity -v -0.5 0.5 --noverbose -c BlueWhiteOrangeRed'.split())
 #view.main('velocityERA5.h5 --notick --noaxis --noverbose'.split())
 
 # %%
