537ef03987fb030c6ca8f1d6f22f1c2187d50867
jaha2600 <jasmine.hansen@colorado.edu>
2022-04-25 19:15:30 -0600

    updates for isce2 install and run

---
+++ isce/tops_stack/isce2install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# written by Joel Johnson joel.johnson@colorado.edu
+# with marked edits by Jasmine Hansen jasmine.hansen@colorado.edu
+#this script installs isce on Summit
+#seperate scripts are needed because there are different gcc compilers on Summit than on Alpine
+
+#load modules you need:
+ml purge
+ml gcc/10.2.0
+ml cmake/3.14.1
+ml anaconda
+
+#create isce2 conda env w/ dependencies (5-10 min). Need if no conda isce2 env exists
+# jh added install of isce2 conda distrib and shapley
+conda create -y -n isce2 -c conda-forge python cython gdal git h5py libgdal pytest numpy fftw scipy basemap scons opencv isce2 shapley
+
+#activate the environment
+conda activate isce2
+
+#now go to your software directory:
+cd /projects/$USER/software/anaconda
+
+#now get and compile isce2
+git clone https://github.com/isce-framework/isce2.git
+mv isce2 isce2_summit
+cd isce2_summit
+export ISCE_BASE_DIR=$PWD
+mkdir local
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${ISCE_BASE_DIR}/local -DCMAKE_PREFIX_PATH=${CONDA_PREFIX}
+make -j 8 install
+
+# add paths directly to bashrc file for executing - jh edit from below
+# the \ make it not confuse the $ for something else.
+# first three paths from oroginal install script
+echo -e "export ISCE_BASE_DIR=/projects/\$USER/software/anaconda/isce2_summit" >> ~/.bashrc
+echo -e "export PATH=\${ISCE_BASE_DIR}/local/bin:\${ISCE_BASE_DIR}/applications:\$PATH" >> ~/.bashrc
+echo -e "export PYTHONPATH=\${ISCE_BASE_DIR}/local/packages:\$PYTHONPATH" >> ~/.bashrc
+# jh add path to stack
+echo -e "export PATH=\${ISCE_BASE_DIR}/local/bin:\${ISCE_BASE_DIR}/contrib/stack/topsStack:\$PATH" >> ~/.bashrc
+
+# add the file to allow automatic orbit downloads
+echo "machine urs.earthdata.nasa.gov login uid_goes_here password password_goes_here" > ~/.netrc
+
+#to run add these lines to your job script:
+### no export lines required as added directly to bashrc from above ###
+#echo ml purge
+#echo ml gcc/10.2.0
+#echo ml anaconda
+#echo conda activate isce2
+#echo export ISCE_BASE_DIR=/projects/$USER/software/anaconda/isce2_summit
+#echo export PATH=${ISCE_BASE_DIR}/local/bin:${ISCE_BASE_DIR}/applications:$PATH
+#echo export PYTHONPATH=${ISCE_BASE_DIR}/local/packages:$PYTHONPATH
+#I think we need to go to this /projects/jojo8550/software/anaconda/isce2/contrib/stack/topsStack/:$PATH
--- isce/tops_stack/summit_stack.sh
+++ isce/tops_stack/summit_stack.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
-### POTENTIAL ISSUES ? ###
-# do i need absolute pathing for files and script locations
-# do we need to change dir so it runs in the directiry with the files in?
-
 # Written by: jaha2600@colorado.edu
 # Date: 20220421
 # Purpose: This script submits a job on RMACC Summit. Can be used with other HPC systems if SBATCH/scheduling commands are changed.   
 #SBATCH --account=      # Summit allocation
 #SBATCH --partition=shas    # Summit partition
 #SBATCH --qos=                # Summit qos
-#SBATCH --time=24:00           # Max wall time
+#SBATCH --time=24:00:00           # Max wall time
 #SBATCH --nodes=1          # Number of Nodes
 #SBATCH --ntasks=24          # Number of tasks per job
 
-#SBATCH --job-name=sstack-       # Job submission name
+#SBATCH --job-name=sstack      # Job submission name
 #SBATCH --mail-type=END            # Email user when job finishes
 #SBATCH --mail-user=jaha2600@colorado.edu # Email address of user
 
 # purge existing modules
-ml purge 
+ml purge
 # load modules
-ml gcc/10.2.0 
-ml anaconda 
-conda activate isce2 
+ml gcc/10.2.0
+ml anaconda
+conda activate isce2
 
 # select files in correct places for ion corr
+# add the correct upper and lower bounds for your dataset
 s1_select_ion.py -dir ./slc -sn 65.505836/67.561638 -nr 10
 
 #then run the stack senintel to make the run files 
+# no need to have an orbits directory as it will download automatically
+# need to have slc/ dem/ aux_dir/
 stackSentinel.py -s slc/ -d dem/glo_30.dem.wgs84 -a aux_dir/ -o orbits/ -b '65.505836 67.561638 -55.494823 -49.108829' -c 4 -p hv --param_ion ./ion_param.txt --num_connections_ion 3
 
+# move to runfiles directory
 cd run_files/
 
 # compile all shell scripts together and add the && symbol at the end to run all together - creates a bash script called batch.sh that you can then execute
 
+# list individual run files
 ls run* > batch.sh
-# add the symbol to the end.
+# add the && symbol to the end.
 sed -i 's/$/\ \&\&/' batch.sh
 #add the ./ to start of each line
 sed -i 's/^/\.\//' batch.sh
@@ -46,6 +47,6 @@ sed -i 's/run_24_invertIon \&\&/run_24_invertIon/' batch.sh
 chmod +x *
 
 # run batch script which executes the scripts in order one after the other
-#./batch.sh 
+./batch.sh
 
 echo 'Script Complete'
