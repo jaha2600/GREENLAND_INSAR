f3238c6a9e4e5e3b93ebab5ea17082fc59d70fc0
jaha2600 <jasmine.hansen@colorado.edu>
2022-04-28 19:47:50 -0600

    optimization

--- isce/tops_stack/summit_stack_par.sh
+++ isce/tops_stack/summit_stack_par.sh
@@ -19,11 +19,56 @@ ml purge
 # load modules
 ml gcc/10.2.0
 ml anaconda
-# load GNU parallel
-ml parallel
 conda activate isce2
 
+# select files in correct places for ion corr
+# add the correct upper and lower bounds for your dataset
+s1_select_ion.py -dir ./slc -sn 65.505836/67.561638 -nr 10
 
-# have individual sbatch submissions for each run file, that don't start until the first one has been executed
+#then run the stack senintel to make the run files 
+# no need to have an orbits directory as it will download automatically
+# need to have slc/ dem/ aux_dir/
+stackSentinel.py -s slc/ -d dem/glo_30.dem.wgs84 -a aux_dir/ -o orbits/ -b '65.505836 67.561638 -55.494823 -49.108829' -c 4 -p hv --param_ion ./ion_param.txt --num_connections_ion 3
 
-# within that you want to paralleize each line in the run file script so that it runs them all together
+# move to runfiles directory
+cd run_files/
+
+### JH ADD AND HAS TESTED ON SUMMIT AND SEEMS TO WORK WELL ###
+### adds echo commands to start an end of each run file to make it easier to see where we get to if fail / timeout 
+ls run* > run_list
+# add text to start of file so you know the section.
+for f in $(cat run_list) ; do sed -i "1s/^/echo '${f} START'\n/" ${f}; done
+# then add the ending text to end of script.
+for i in $(cat run_list) ; do echo "echo '${i} END'" >> ${i} ; done 
+
+# now submit sbatch runs for each run file, but make sure it holds the first one until the next
+
+# be sure to put wait flags in the sbatch header and parallel things
+# create an example batch file with and without parallization
+sbatch run_01.sh 
+sbatch run_02.sh
+sbatch run_03.sh
+sbatch run_04.sh
+sbatch run_05.sh
+sbatch run_06.sh
+sbatch run_07.sh
+sbatch run_08.sh
+sbatch run_09.sh
+sbatch run_10.sh
+sbatch run_11.sh
+sbatch run_13.sh
+sbatch run_14.sh
+sbatch run_15.sh
+sbatch run_16.sh
+sbatch run_17.sh
+sbatch run_18.sh
+sbatch run_19.sh
+sbatch run_20.sh
+sbatch run_21.sh
+sbatch run_22.sh
+sbatch run_23.sh
+sbatch run_24.sh
+
+
+
+
