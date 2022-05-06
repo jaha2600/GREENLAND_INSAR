873498817c5d091b1030cbae7b70ebd65520acbc
jaha2600 <jasmine.hansen@colorado.edu>
2022-05-06 12:21:45 -0600

    andy m summit

---
+++ isce/tops_stack/rubmit_runfiles_jobs_v2.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+ i=1
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=$(wc -l run_files/${EXE_SCRIPT} | awk '{print $1}')
+ NUM_TASKS=$(echo $((NUM_TASKS<24 ? NUM_TASKS : 24)))
+ id=`sbatch --export=NONE --partition=shas --qos=normal --account=ucb-summit-mjw --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=$NUM_TASKS --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submitted initial job $EXE_SCRIPT as $id with $NUM_TASKS ntasks"
+
+ i=2
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=$(wc -l run_files/${EXE_SCRIPT} | awk '{print $1}')
+ NUM_TASKS=$(echo $((NUM_TASKS<24 ? NUM_TASKS : 24)))
+ id2=$id
+ id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account=ucb-summit-mjw --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=$NUM_TASKS --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with $NUM_TASKS ntasks, which depends on job $id2"
+
+ i=3
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=$(wc -l run_files/${EXE_SCRIPT} | awk '{print $1}')
+ NUM_TASKS=$(echo $((NUM_TASKS<24 ? NUM_TASKS : 24)))
+ id2=$id
+ id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account=ucb-summit-mjw --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=$NUM_TASKS --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with $NUM_TASKS ntasks, which depends on job $id2"
+
+ i=4
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=$(wc -l run_files/${EXE_SCRIPT} | awk '{print $1}')
+ NUM_TASKS=$(echo $((NUM_TASKS<24 ? NUM_TASKS : 24)))
+ id2=$id
+ id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account=ucb-summit-mjw --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=$NUM_TASKS --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with $NUM_TASKS ntasks, which depends on job $id2"
