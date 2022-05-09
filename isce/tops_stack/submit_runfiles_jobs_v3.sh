05bd293109b4bbe8c75d3667d002c7beeb980c9d
jaha2600 <jasmine.hansen@colorado.edu>
2022-05-09 15:05:16 -0600

    .

--- isce/tops_stack/submit_runfiles_jobs_v3.sh
+++ isce/tops_stack/submit_runfiles_jobs_v3.sh
@@ -20,7 +20,6 @@
 
  i=4
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
-
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=24 --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with 24 ntasks, which depends on job $id2" 
