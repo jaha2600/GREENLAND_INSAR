c6a3971f37e0bffad14e8d1510c413cbfc8aca44
jaha2600 <jasmine.hansen@colorado.edu>
2022-05-10 09:09:03 -0600

    changes

--- isce/tops_stack/submit_runfiles_jobs_v3.sh
+++ isce/tops_stack/submit_runfiles_jobs_v3.sh
@@ -10,162 +10,161 @@
 
  i=2
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=3
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=4
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
  
  i=5
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=6
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=7
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=8
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
  
  i=9
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=10
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=11
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=12
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
  
  i=13
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=14
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=15
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=16
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
  
  i=17
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=18
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=19
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=20
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=$(wc -l run_files/${EXE_SCRIPT} | awk '{print $1}')
- NUM_TASKS=$(echo $((NUM_TASKS<24 ? NUM_TASKS : 24)))
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
  
  i=21
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=22
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=23
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
 
  i=24
  EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
- NUM_TASKS=24
+ NUM_TASKS=12
  id2=$id
  id=`sbatch --depend=afterany:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
  echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
