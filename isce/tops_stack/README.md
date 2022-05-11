d99346f2febdad864d96fb6e5b72878fa03fa9fa
jaha2600 <jasmine.hansen@colorado.edu>
2022-05-11 14:29:50 -0600

    readme clarifcations and tidying up

--- isce/tops_stack/README.md
+++ isce/tops_stack/README.md
@@ -36,7 +36,16 @@ Navigate to run_files directory. Place `runfile_jobs.sh` and `submit_runfiles_jo
 
 Submit `./submit_runfiles_jobs_v3.sh` in commandline - not using sbatch command but ensure you are on an scompile node.
 
-USER CHANGES: check `--account`, `--nodes` and other associated slurm commands. Check number of stages is equal to number of run_file changes in the directory. For each stage change $NUM_TASKS to match what you want it to be. Some information about jobtime vs ntasks can be found in the top of the script. 
+USER CHANGES: check `--account`, `--nodes` and other associated slurm commands. Check number of stages is equal to number of run_file changes in the directory. For each stage change `$NUM_TASKS` to match what you want it to be. Some information about jobtime vs ntasks can be found in the top of the script. 
 
+File Variations:
+
+There a few variations of the submit_runfiles and runfiles_jobs scripts in the directory which are mostly related to trying to find the most optimum balance between job speed and scheduling. Below I outline what the differences are:
+
+`submit_runfiles_jobs_abs.sh` This is a version of submit_runfiles_jobs that can be run from your /projects/$USER directory, you place the path to the isce stack directory at the top. It has to copy runfiles_jobs.sh into the isce stack directory as that cannot be absolutely pathed.
+
+`submit_runfiles_jobs_total.sh` This hasn't been tested yet but it just integrates the prep_stack_par.sh stage into the bulk submission of jobs. Again place prep_stack_par.sh in the isce stack directory. Run_files stage 01 should then run upon successful completion of this stage.
+
+`runfiles_jobs_2.sh` This uses two cpus per task for GNU parallel to improve memory when running computationally heavy stages. This may end up being the default runfiles_jobs script. Using two cpus per task ONLY works it --ntasks is equal to or less than 12. If you do use this script remember to change the name in the submit_runfiles script. 
 
 ### If code used in publication please reference this github, isce2, and the associated authors listed in each script.
---
+++ isce/tops_stack/submit_runfiles_jobs_total.sh
@@ -0,0 +1,194 @@
+#!/bin/sh
+## Written by Andy Monaghan, Joel Johnson & Jasmine Hansen ##
+## May 9th 2022 JH edit to manually add --ntasks and add max stage numbers
+
+### Below are job lengths for a 10 interferogram test set (still running at present).
+# run_01 - 24 ntasks - 44 min, run_02 - 12 ntasks - 46 sec, run_03 - 12 ntasks - 15 sec, run_04 - 12 ntasks - 10 sec, run_05 - 12 ntasks - 25 min
+# run_06 - 12 ntasks - 50 min, run_07 - 12 ntasks - 55 min, run_08 - 12 ntasks - 10 sec, run_09 - 12 ntasks - 03 hrs, run_10 - 12 ntasks - 01 hr 14 min
+# run_11 - 12 ntasks - 02 min, run_12 - 12 ntasks - 04 hrs, run_13 - 12 ntasks - 14 min, run_14, run_15, run_16 - 12 ntasks - MEM FAIL
+
+##### experimental - add the prep stage into the start and make the run_01 stage dependent on that job and then continue as normal #####
+# submit prep stage (needs to be in same dir as submit_runfiles script)
+i=0
+id=`sbatch --export=NONE --partition=shas --qos=normal --account= --job-name=prep_stage --output=prep_output_%j.out --nodes=1 --ntasks=6 --time=24:00:00 prep_stack_par.sh | awk '{print $4}'`
+echo "submitted prep stage as $id with 6 ntasks"
+
+# now submit the run_01 with the added dependency of the prep stage needing to run first
+ i=1
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=24
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submitted initial job $EXE_SCRIPT as $id with $NUM_TASKS ntasks, which depends on job $id2"
+
+
+ #i=1
+ #EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ #NUM_TASKS=24
+
+ #id=`sbatch --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ #echo "submitted initial job $EXE_SCRIPT as $id with $NUM_TASKS ntasks"
+ 
+ i=2
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=3
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=4
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
+ 
+ i=5
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=6
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=7
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=8
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
+ 
+ i=9
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=10
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=11
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=12
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
+ 
+ i=13
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=14
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=15
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=16
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
+ 
+ i=17
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=18
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=19
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=20
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2" 
+ 
+ i=21
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=22
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=23
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+
+ i=24
+ EXE_SCRIPT=$(sed -n "${i}p" run_files/run_list)
+ NUM_TASKS=12
+ id2=$id
+ id=`sbatch --depend=afterok:$id2 --export=NONE --partition=shas --qos=normal --account= --job-name=${EXE_SCRIPT} --output=${EXE_SCRIPT}_output_%j.out --nodes=1 --ntasks=${NUM_TASKS} --time=24:00:00 ./runfiles_jobs.sh | awk '{print $4}'`
+ echo "submited job $EXE_SCRIPT as $id with ${NUM_TASKS} ntasks, which depends on job $id2"
+ 
+ 
+ 
